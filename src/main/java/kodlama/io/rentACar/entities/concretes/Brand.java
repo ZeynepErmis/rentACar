952286a9cb835a197fa073380649cd3f9f699e57
ZeynepErmis <zzeyneppermis@gmail.com>
2024-03-27 14:53:43 +0000

    sql connection

--- src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
+++ src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -1,31 +1,32 @@
 package kodlama.io.rentACar.entities.concretes;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "brands")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Brand {
+
+	// primary key
+	@Id
+	// increase ID one by one
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	// column name in table
+	@Column(name = "id")
 	private int id;
+
+	@Column(name = "name")
 	private String name;
-
-	public Brand() {
-	}
-
-	public Brand(int id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
 }
--- src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
+++ src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlama.io.rentACar.business.abstratcs.BrandService;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
 import kodlama.io.rentACar.entities.concretes.Brand;
 
 //annotation
@@ -20,9 +23,14 @@ public class BrandsController {
 	public BrandsController(BrandService brandService) {
 		this.brandService = brandService;
 	}
-	
+
 	@GetMapping("/getall")
-	public List<Brand> getAll() {
+	public List<GetAllBrandsResponse> getAll() {
 		return brandService.getAll();
+	}
+
+	@PostMapping("/add")
+	public void add(CreateBrandRequest createBrandRequest) {
+		this.brandService.add(createBrandRequest);
 	}
 }
