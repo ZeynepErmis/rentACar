227be6346358fac2d4987f7a67293ae7672074f6
ZeynepErmis <zzeyneppermis@gmail.com>
2024-04-03 23:06:16 +0100

    model mapper config

--- src/main/java/kodlama/io/rentACar/RentACarApplication.java
+++ src/main/java/kodlama/io/rentACar/RentACarApplication.java
@@ -1,7 +1,9 @@
 package kodlama.io.rentACar;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RentACarApplication {
@@ -10,4 +12,8 @@ public class RentACarApplication {
 		SpringApplication.run(RentACarApplication.class, args);
 	}
 
+	@Bean
+	ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
 }
